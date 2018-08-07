23910d227d239975d6f38f83ae258d3fe36cb51c
sumitjoshi97 <sumitjoshi479@gmail.com>
2018-08-07 11:24:34 +0530

    added favorite beers

--- src/containers/Favorites/Favorites.jsx
+++ src/containers/Favorites/Favorites.jsx
@@ -1,13 +1,33 @@
 import React, { Component } from 'react'
+import { connect } from 'react-redux';
 
 export class Favorites extends Component {
-  render() {
-    return (
-      <div>
-        your favorite beer
-      </div>
-    )
-  }
+
+    render() {
+        const favList = this.props.favorites.map(favorite => {
+            return (
+                <div className="beers__list__beer" key={favorite.id}>
+                    <div className="beers__list__beer--image">
+                        <img src={favorite.image_url} alt="" /></div>
+                    <div className="beers__list__beer--name">{favorite.name}</div>
+                </div>
+            )
+
+        })
+        return (
+            <div className="beers">
+                <div className="beers__list">
+                    {favList}
+                </div>
+
+            </div>
+        )
+    }
 }
 
-export default Favorites
+const mapStateToProps = state => {
+    return {
+        favorites: state.favorites
+    }
+}
+export default connect(mapStateToProps, null)(Favorites);
